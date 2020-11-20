f6cb32cf17eef709c577339617294230e9d17322
Yvette <yvette@Yvettes-MacBook-Pro.local>
2020-11-20 09:32:02 -0800

    updated js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # password-generator
+The piece generates a password based on confirmation from users by pressing the generate password button.
+The passwords is between 8 and 128 charaters that can contain upper case letters, lower case letters, numbers and special characters.
--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var special = "!@#$%^&*";
-var number = "0123456789";
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lower = "abcdefghijklmnopqrstuvwxyz";
+var SPECIAL = "!@#$%^&*";
+var NUMBER = "0123456789";
+var UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var LOWER = "abcdefghijklmnopqrstuvwxyz";
+
 
 
   console.log(generateBtn);
@@ -24,43 +25,42 @@ var lower = "abcdefghijklmnopqrstuvwxyz";
   console.log(passwordLength);
    while(parseInt(passwordLength) < 8
   || parseInt(passwordLength) > 128){
-  passwordLength = prompt("You must have a minium of 8 and a maxium of 128.  Please re-enter length of password");  
+  passwordLength = prompt("You must have a min of 8 and a max of 128.  Please re-enter length of password");  
    }
    
-  var lower= confirm("Would you like lowercase letters?");
-  if(lower){
+  var lowerCase= confirm("Would you like lowercase letters?");
+  if(lowerCase){
     var password ="Your password is "
-    var chars = passwordLength + lower;
-  }
-
-
-  var upper = confirm("Would you like uppercase letters?");
-  if(upper){
-  var chars = passwordLength + lower + upper;
-  }
-
-  var number = confirm("Would you like  numbers?");
-  if(number){
-    var chars = passwordLength + lower + upper + number;
+    var chars = passwordLength + LOWER;
   }
   
-  var special =  confirm("Would you like  special characters?");
-  if( special){
-    var chars = passwordLength + lower + upper + number + special;
-    
-  }
+
+  var upperCase = confirm("Would you like uppercase letters?");
+  if(upperCase){
+  var chars = passwordLength + LOWER + UPPER;
+  console .log(chars);
+  } 
+
+  var numberChar = confirm("Would you like  numbers?");
+  if(numberChar){
+    var chars = passwordLength + LOWER + UPPER + NUMBER;
+  } 
+  
+  var specialChar =  confirm("Would you like  special characters?");
+  if(specialChar){
+    var chars = passwordLength + LOWER + UPPER + NUMBER + SPECIAL; 
+  }  
    
   for(var i = 0; i < passwordLength; i++){
     var random = Math.floor(Math.random() * chars.length);
     password = password + chars[random]; 
-    
+   
   }
   
   return password;
           
      
   }
-  
 generateBtn.addEventListener("click", writePassword);
 
 
